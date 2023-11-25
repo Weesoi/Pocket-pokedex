607b6b7e3e9a9b9386433d28ab4299b61a11150f
Weesoi <112541078+Weesoi@users.noreply.github.com>
2023-11-25 14:26:50 +0200

    dfds

--- scripts.js
+++ scripts.js
@@ -7,5 +7,10 @@ var PokeRequest = new XMLHttpRequest();
 PokeRequest.open('GET', url, true);
 
 
+// tällä saadaan pokemon extry teksti
+// GET https://pokeapi.co/api/v2/pokemon-species/25
 
+
+
+//tee suprise me- nappi jos kerkeät
 }
