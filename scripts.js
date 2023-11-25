6865312bb06ec6ca7f1870399800357ece2a1917
Weesoi <112541078+Weesoi@users.noreply.github.com>
2023-11-25 15:20:21 +0200

    sadas

--- scripts.js
+++ scripts.js
@@ -8,9 +8,18 @@ PokeRequest.open('GET', url, true);
 
 
 // tällä saadaan pokemon extry teksti
-// GET https://pokeapi.co/api/v2/pokemon-species/25
+// GET https://pokeapi.co/api/v2/pokemon-species/
 
-
+//halutut tiedot: types, entry, name+ pokedex number , height, abilities, weight
 
 //tee suprise me- nappi jos kerkeät
 }
+
+
+
+// https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/87.svg 
+// täältä spritet
+
+
+//täältä loput:
+//https://github.com/favware/graphql-pokemon#using-fetch
