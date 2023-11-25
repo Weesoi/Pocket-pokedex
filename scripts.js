c016ee709e275f70de00ad6b81b8d55a01fdc59f
Weesoi <112541078+Weesoi@users.noreply.github.com>
2023-11-25 16:50:10 +0200

    fsdf

--- scripts.js
+++ scripts.js
@@ -1,10 +1,43 @@
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+// Luo uusi XMLHttpRequest-objekti
+const xhr = new XMLHttpRequest();
+
+// Määritä pyyntötyyppi ja kohde
+xhr.open("GET", apiUrl, true);
+
+// Määritä tapahtumakäsittelijä, kun pyyntö on valmis
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    if (xhr.status === 200) {
+      // Käsittele vastaus tässä
+      const data = JSON.parse(xhr.responseText);
+      console.log(data);
+    } else {
+      // Jos pyyntö epäonnistuu, tulosta virheilmoitus
+      console.error("There was an error with the XMLHttpRequest. Status:", xhr.status);
+    }
+  }
+};
+
+// Lähetä pyyntö
+xhr.send();
+
+
+
+
+
+
+
 function getPokemonInfo() {
+
+pokeContainer.innerHTML = "";
 
 url = "https://pokeapi.co/api/v2/pokemon/ditto"
 
 
 var PokeRequest = new XMLHttpRequest();
-PokeRequest.open('GET', url, true);
+PokeRequest.open('GET', "https://pokeapi.co/api/v2/pokemon/", true);
 
 
 // tällä saadaan pokemon extry teksti
