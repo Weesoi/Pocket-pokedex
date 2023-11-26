51c7876014c78cc8eea955d82fe68b7d2b053efd
Weesoi <112541078+Weesoi@users.noreply.github.com>
2023-11-26 12:36:08 +0200

    sdssd

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 } 
                 }
             }
-        };
+        });
 
         // Lähetä pyyntö
         //pyynto.send();
